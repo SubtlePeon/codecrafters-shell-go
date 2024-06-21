0a1b461d7824a4d990f850ccf3252228b4f48bab
Example <example@example.com>
2024-06-20 21:47:01 -0400

    Exit functionality

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -15,13 +16,43 @@ func main() {
 		// Wait for user input
 		s, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			_ = fmt.Errorf(err.Error())
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
 		}
 		s = strings.TrimSpace(s)
 
+		cmd := strings.Split(s, " ")
+
+		for i := len(cmd) - 1; i >= 0; i-- {
+			if cmd[i] == "" {
+				cmd = append(cmd[:i], cmd[i+1:]...)
+			}
+		}
+
 		// All commands are unknown to us
-		if true {
+		if len(cmd) == 0 {
+			// Just pass
+		} else if cmd[0] == "exit" {
+			handle_exit(cmd[1:])
+		} else {
 			fmt.Printf("%s: command not found\n", s)
 		}
+	}
+}
+
+// Handles the `exit` command. The arguments after "exit" should be split and
+// passed in.
+func handle_exit(cmd []string) {
+	if len(cmd) == 0 {
+		os.Exit(0)
+	} else if len(cmd) == 1 {
+		num, err := strconv.ParseInt(cmd[0], 10, 32)
+		if err != nil {
+			fmt.Printf("exit: could not parse exit code\n%v\n", err)
+			return
+		}
+		os.Exit(int(num))
+	} else {
+		fmt.Println("exit: too many arguments")
 	}
 }
